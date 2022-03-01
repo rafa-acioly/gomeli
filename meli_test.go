5e74cf32244d1b76c6fc9892773dc656cc9ae9b6
Rafael Acioly <aciolyr@gmail.com>
2022-03-01 10:06:13 -0300

    adds test for meli client

--- meli_test.go
+++ meli_test.go
@@ -1,1 +1,33 @@
 package meli
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"meli/environments"
+	"meli/storage"
+	"testing"
+)
+
+type MeliTestSuite struct {
+	config environments.Configuration
+	cache  storage.Storage
+	env    environments.Environment
+	suite.Suite
+}
+
+func (test MeliTestSuite) SetupTest() {
+	test.cache = storage.NewInMemoryCache()
+	test.config = environments.NewConfiguration(test.cache)
+	test.env = environments.NewSandboxEnv(environments.BRASIL, test.config)
+}
+
+func (test MeliTestSuite) CanCreateAClientInstance() {
+	cli := NewClient("client-id", "client-secret", test.env)
+
+	assert.Equal(test.T(), cli.GetClientID(), "client-id")
+	assert.Equal(test.T(), cli.GetClientSecret(), "client-secret")
+}
+
+func TestMeliTestSuite(t *testing.T) {
+	suite.Run(t, new(MeliTestSuite))
+}
